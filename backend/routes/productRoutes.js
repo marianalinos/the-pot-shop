aac023259004fb60051626fb212a96b5ec117c8e
mariana <marianalinosdev@gmail.com>
2025-04-26 22:39:57 -0300

    feat: adicionando routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,10 @@ import * as productController from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', productController.getAllProducts);
-
+router.get('/', productController.getProducts);
+router.get('/:id', productController.getProduct);
 router.post('/', productController.createProduct);
-
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 export default router;
