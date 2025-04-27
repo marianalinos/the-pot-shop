979873063e9971ea52638149f2cb5f85aa6da42d
mariana <marianalinosdev@gmail.com>
2025-04-27 01:40:11 -0300

    feat: segurança

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,33 @@ app.use(cors());
 app.use(helmet()); // middleware de segurança que ajuda a proteger o app definindo cabeçalhos HTTP
 app.use(morgan("dev")); // log requests
 
+// aplicar arcjet rate-limiting para todas as rotas
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many requests." });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bots are not allowed." });
+      } else {
+        res.status(403).json({ error: "Access denied." });
+      }
+      return;
+    }
+    //check for spoofed bots
+    if(decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected." });
+      return;
+    }
+  } catch (error) {
+    console.error("Error in arcjet middleware: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.use("/api/products", productRoutes);
 
 async function initDB() {
